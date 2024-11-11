0bdd66e82082e55c418fe4c8a12112ddce6c8b8b
toyoda-toppo <186126032+toyoda-toppo@users.noreply.github.com>
2024-11-11 14:56:13 +0900

    backボタンをtopに戻るに変更

--- view/headerAdmin.php
+++ view/headerAdmin.php
@@ -1,5 +1,5 @@
 <header>
-    <button id="backButton" onclick="location.href='manageTop.php'">＜ Back</button>
+    <button id="backButton" onclick="location.href='manageTop.php'">管理者Top画面へ</button>
     <h1><?php echo $vAry['h1Title']; ?></h1>
     <a href="main.php" class="logout">ログアウト</a>
 </header>
