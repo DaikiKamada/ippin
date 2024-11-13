7c61b4102a8c343dcef0e7a13e699444e2e58adf
DaikiKamada <130561768+nagoshi19th@users.noreply.github.com>
2024-11-13 14:58:56 +0900

    ippinカードの表示項目を調整しました^^b

--- view/ippinResult.php
+++ view/ippinResult.php
@@ -2,181 +2,198 @@
     <div class="ippin_select_box container text-center">
         <h2 class="ippin_result_title">つくれるippin</h2>
         <p class="ippin_result_text">選択中の食材</p>
-        <div class="select_foods_box">
-            <button class="select_foods_btn btn btn--orange btn--radius">食材1</button>
-            <button class="select_foods_btn btn btn--orange btn--radius">食材2</button>
-            <button class="select_foods_btn btn btn--orange btn--radius">食材3</button>
+        <div class="select_foodValeus_box">
+            <button class="select_foodValeus_tag">食材1</button>
+            <button class="select_foodValeus_tag">食材2</button>
+            <button class="select_foodValeus_tag">食材3</button>
         </div>
-        <p>※下のメニューをクリックするとレシピページにジャンプします</p>
+        <p class="ippin_result_annotation">※下のメニューをクリックするとレシピページにジャンプします</p>
     </div>
 
     <div class="container">
         <div class="row">
             <!-- ippin card -->
             <!-- ↓↓↓PHPが入ります！↓↓↓ -->
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://cookpad.com/jp/recipes/24191546-%E3%81%9F%E3%81%A3%E3%81%B7%E3%82%8A%E9%BA%BB%E5%A9%86%E8%B1%86%E8%85%90?ref=search&search_term=%E9%BA%BB%E5%A9%86%E8%B1%86%E8%85%90" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe01.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe01.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">まぁぼぉどうふ</h5>
-                            <p class="card-text">使う食材：<button class="select_foods_btn_s btn btn--orange btn--radius">食材1</button></p>
+                            <p class="card-siteName">
+                                出典：
+                                kmd-pad
+                            </p>
+                            <h5 class="card-recipeName">まぁぼぉどうふ</h5>
+                            <p class="card-foodValues">
+                                使う食材：
+                                <button class="foodValues_tag">食材1</button>
+                                <button class="foodValues_tag">食材2</button>
+                                <button class="foodValues_tag">食材3</button>
+                            </p>
+                            <p class="card-memo">
+                                補足：
+                                豆板醤と麻辣醤があるとよりよい
+                            </p>
+                            <p class="card-comment">
+                                コメント：
+                                四川風のやつと広東風のやつがあります
+                            </p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://cookpad.com/jp/recipes/21647486-%E3%83%81%E3%83%BC%E3%82%BA%E3%83%95%E3%82%A9%E3%83%B3%E3%83%87%E3%83%A5?ref=search&search_term=%E3%83%81%E3%83%BC%E3%82%BA%E3%81%BB%E3%82%93%E3%81%A7%E3%82%85" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe02.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe02.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">ちぃずふぉんでゅ</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">ちぃずふぉんでゅ</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://oceans-nadia.com/user/17/recipe/101096" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe03.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe03.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">たまかけとりそぼろごはん</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">たまかけとりそぼろごはん</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://www.sirogohan.com/recipe/omuraisukihon/" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe04.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe04.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">おむらいす</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">おむらいす</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://www.marukome.co.jp/recipe/detail/daizu_411/" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe05.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe05.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">みそらぁめん</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">みそらぁめん</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://delishkitchen.tv/recipes/166742173524427155" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe06.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe06.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">はんばぁぐ</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">はんばぁぐ</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://nishikiya-shop.com/column/143" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe07.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe07.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">こぉんぽたぁじゅ</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">こぉんぽたぁじゅ</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://www.orangepage.net/recipes/detail_135592" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe08.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe08.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">ひややっこ</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">ひややっこ</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://www.nipponham.co.jp/recipes/kazarigiri/octopus.html" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe09.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe09.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">たこうぃんなぁさん</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">たこうぃんなぁさん</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://park.ajinomoto.co.jp/recipe/card/708236/" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe10.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe10.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">たたききゅうり</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">たたききゅうり</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://www.buzzfeed.com/jp/mayumioowada/baked-potato-butter" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe11.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe11.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">じゃがばたぁ</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">じゃがばたぁ</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card">
                     <a href="https://www.tabechoku.com/products/115505" target="_blank">
                         <div class="image-container">
-                            <img src="images/recipe12.jpg" class="card-img-top" alt="...">
+                            <img src="images/recipe12.jpg" class="card-img" alt="...">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">にんじん</h5>
-                            <p class="card-text"></p>
+                            <h5 class="card-recipeName">にんじん</h5>
+                            <p class="card-foodValues"></p>
                         </div>
                     </a>
                 </div>
