135604bcf763321024e28b4296f397fed2c80d0e
fukumoto <186124459+fukumotoA@users.noreply.github.com>
2024-11-15 14:26:32 +0900

    foodIdのソート部分を修正

--- other/control.php
+++ other/control.php
@@ -13,20 +13,19 @@ $result = $obj->insertFoodM($foodInfo);
 
 
 // 日時を取得して連想配列に追加
-$_POST = ['recipeName' => 'カプレーゼ2', 'foodId1' => 2, 'foodId2' => 3, 'foodId3' => 1, 'url' => 'url', 'howtoId' => 1, 'comment' => 'コメント',  'memo' => 'メモ', 'img' => 'img', 'userId' => 1, 'siteName' => 'サイトネーム'];
-print_r ($_POST);
+$foodIds = [2, 3, 1];
+$_POST = ['recipeName' => 'カプレーゼ2', 'foodIds' => $foodIds, 'url' => 'url', 'howtoId' => 1, 'comment' => 'コメント',  'memo' => 'メモ', 'img' => 'img', 'userId' => 1, 'siteName' => 'サイトネーム'];
+// print_r ($_POST);
 $recipeInfo = $_POST;
 
 $lastUpdate = getDatestr();
 $recipeInfo['lastUpdate'] = $lastUpdate;
 
 // foodIdをソートして連想配列に追加
-$foodIds = foodIdsinArr($recipeInfo);
-// print_r ($foodIds);
-$foodValues = sortFoodIds($foodIds);
+$foodValues = sortFoodIds($recipeInfo['foodIds']);
 // print $foodValues;
 $recipeInfo['foodValues'] = $foodValues;
-print_r($recipeInfo);
+// print_r($recipeInfo);
 
 // recipe版：POSTした内容を連想配列に代入
 // function foodIdsinArr(array $recipeInfo):array {
