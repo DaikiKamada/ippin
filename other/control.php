5a01f2bfbdd17bde245ada215b83270186ccc373
fukumoto <186124459+fukumotoA@users.noreply.github.com>
2024-11-15 14:22:37 +0900

    関数foodIdsinArrが不要だったため削除

--- other/control.php
+++ other/control.php
@@ -13,7 +13,7 @@ $result = $obj->insertFoodM($foodInfo);
 
 
 // 日時を取得して連想配列に追加
-$_POST = ['recipeName' => 'カプレーゼ1', 'foodId1' => 2, 'foodId2' => 3, 'foodId3' => 1, 'url' => 'url', 'howtoId' => 1, 'comment' => 'コメント',  'memo' => 'メモ', 'img' => 'img', 'userId' => 1, 'siteName' => 'サイトネーム'];
+$_POST = ['recipeName' => 'カプレーゼ2', 'foodId1' => 2, 'foodId2' => 3, 'foodId3' => 1, 'url' => 'url', 'howtoId' => 1, 'comment' => 'コメント',  'memo' => 'メモ', 'img' => 'img', 'userId' => 1, 'siteName' => 'サイトネーム'];
 print_r ($_POST);
 $recipeInfo = $_POST;
 
@@ -29,19 +29,19 @@ $recipeInfo['foodValues'] = $foodValues;
 print_r($recipeInfo);
 
 // recipe版：POSTした内容を連想配列に代入
-function foodIdsinArr(array $recipeInfo):array {
-    $foodIds = [];
-    if (is_int($recipeInfo['foodId1'])) {
-        $foodIds[] = $recipeInfo['foodId1'];
-    }
-    if (is_int($recipeInfo['foodId2'])) {
-        $foodIds[] = $recipeInfo['foodId2'];
-    }
-    if (is_int($recipeInfo['foodId3'])) {
-        $foodIds[] = $recipeInfo['foodId3'];
-    }
-    return $foodIds;
-}
+// function foodIdsinArr(array $recipeInfo):array {
+//     $foodIds = [];
+//     if (is_int($recipeInfo['foodId1'])) {
+//         $foodIds[] = $recipeInfo['foodId1'];
+//     }
+//     if (is_int($recipeInfo['foodId2'])) {
+//         $foodIds[] = $recipeInfo['foodId2'];
+//     }
+//     if (is_int($recipeInfo['foodId3'])) {
+//         $foodIds[] = $recipeInfo['foodId3'];
+//     }
+//     return $foodIds;
+// }
 
 // テスト用
 // $obj = new InsertSql('insert処理', 0);
