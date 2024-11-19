eb6d1629a84dc8ea47647d1b7698570eb9b299c3
cNagata-Code <186126038+cNagata-Code@users.noreply.github.com>
2024-11-19 15:15:39 +0900

    SelectSql：選択された食材リストを取得するメソッド(getSelectedFood)を追加 @nagata

--- common/SelectSql.php
+++ common/SelectSql.php
@@ -113,5 +113,29 @@ class SelectSql {
         return $sqlTxt;
     }
 
+    // 選択された食材の食材マスタを取得
+    // 戻り値　処理成功：配列　｜　エラー：ResultController
+    public function getSelectedFood(array $fValues): mixed {
+        // 選択された食材IDをSQL用に加工
+        $txt = '';
+        foreach ($fValues as $id) {
+            $txt .= $id.', ';
+        }
+        $txt = substr($txt, 0, strlen($txt) - 2);
+        // SQL分を生成
+        $sql = "SELECT foodId, foodName, catName 
+            FROM `foodm` JOIN `foodcatm` 
+            ON `foodm`.`foodCatId` = `foodcatm`.`foodCatId`
+            WHERE foodId IN ($txt);";
+        $stt = $this->db->prepare($sql);
+        
+        // SQL実行結果をチェック
+        if ($stt->execute()) {
+            return $stt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            $this->msgTxt = '食材マスタデータが取得できません';
+            return new ResultController(0, $this->msgTitle, $this->msgTxt, $this->linkId);
+        }
+    }
 
 }
