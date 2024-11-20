c90bf27b1a2b56e8933489e061b0621e624b5220
fukumoto <186124459+fukumotoA@users.noreply.github.com>
2024-11-20 13:34:58 +0900

    食材を追加できるようにしたよ

--- foodsManagement.php
+++ foodsManagement.php
@@ -7,15 +7,37 @@ require_once "common/insertSql.php";
 require_once 'common/Utilities.php';
 
 // insert(追加ボタンを押した場合の処理)
-// if() {
+if(array_key_exists('insert',$_POST)) {
 
-// }
+    // POSTの内容を$foodInfoにコピー
+    $foodInfo = $_POST;
+    $foodInfo['userId'] = $_SESSION['viewAry']['userId'];
+
+    // 追加処理
+    $obj = new InsertSql('食材の追加処理', 0);
+    $foodsList = $obj->insertFoodM($foodInfo);
+
+    // $_POSTを初期化
+    $_POST = array();
+
+    // 処理結果
+    $result = $foodsList->getResult();
+    if($result['resultNo'] == 0) {
+        // 追加できなかったよというJSのアラートがほしいな～
+    }
+    else {
+        // 追加しましたよというJSのアラートがほしいな～
+    }
+
+}
+else {
+    $recipeInfo = [];
+}
 
 
 // 食材一覧を取得
 $obj = new SelectSql('食材一覧を取得', 0);
 $foodsList = $obj->getFood();
-
 
 
 $vi = new View();
@@ -25,6 +47,7 @@ $vi->setAssign("cssPath", "css/admin.css");
 $vi->setAssign("bodyId", "foodsManagement");
 $vi->setAssign("h1Title", "食材マスタ管理画面");
 $vi->setAssign("main", "foodsManagement");
+$vi->setAssign("userId", 1);
 $vi->setAssign("foodsList", $foodsList);
 
 $_SESSION['viewAry'] = $vi->getAssign();
@@ -32,15 +55,11 @@ $_SESSION['viewAry'] = $vi->getAssign();
 $vi ->screenView("templateAdmin");
 
 // デバッグ用※あとで消そうね！
-echo '<pre>';
+// echo '<pre>';
 
-// echo '$_SESSIONの配列';
-// print_r($_SESSION['viewAry']['recipeList']);
-// echo '$_SESSIONの配列';
-// print_r($_SESSION);
-// echo '$resultの配列';
-// print_r($result);
-echo '$foodsListの配列';
-print_r($foodsList);
+// echo '$foodsListの配列';
+// print_r($foodsList);
+// echo '$_POSTの配列';
+// print_r($_POST);
 
-echo '</pre>';
+// echo '</pre>';
--- view/foodsManagement.php
+++ view/foodsManagement.php
@@ -1,37 +1,24 @@
 <main>
-    <form action="#" method="post" class="new">
+    <form action="foodsManagement.php" method="post" class="new">
         <div>
             食材名：<input type="text" name="foodName" id="foodName">
         </div>
         <div>
             食材分類：
             <select name="foodCatId">
-                <option value="1">肉</option>
-                <option value="2">野菜</option>
+                <option value="1">野菜</option>
+                <option value="2">肉・魚</option>
                 <option value="3">その他</option>
             </select>
         </div>
-        <button type="submit" id="submitBtn" class="disabled" disabled>追加</button>
+        <button type="submit" id="submitBtn" class="disabled" name="insert" disabled>追加</button>
     </form>
     <hr>
     <form method="post" id="fmTable">
         <div class="fM_containor">
             <table class="fM">
                 <tr><th>ID</th><th>食材</th><th>カテゴリ</th><th>recipe件数</th><th>表示</th><th>非表示</th><th>操作</th></tr>
-                <!-- サンプル行 -->
-                <!-- <tr>
-                    <td>1</td>
-                    <td>トマト</td>
-                    <td>野菜</td>
-                    <td>120</td>
-                    <td>100</td>
-                    <td>20</td>
-                    <td>
-                        <button class="edit">編集</button>
-                        <button class="delete">削除</button>
-                    </td>
-                </tr> -->
-
+                
                 <!-- 食材一覧を表示 -->
                 <?php
                 if (isset($vAry['foodsList'])) {
@@ -47,22 +34,36 @@
                         <td><?=$foodsList[$i]['foodId']?></td>
                         <td><?=$foodsList[$i]['foodName']?></td>
                         <td><?=$foodsList[$i]['catName']?></td>
-
+                        
                         <!-- 調整予定 -->
                         <td>120</td>
                         <td>100</td>
                         <td>20</td>
-                    
+                        
                         <td>
                             <button class="edit">編集</button>
                             <button class="delete">削除</button>
                         </td>
-
+                        
                     </tr> <?php
                 }
                 ?>
 
-            </table>
-        </div>
-    </form>
+                <!-- サンプル行 -->
+                <!-- <tr>
+                    <td>1</td>
+                    <td>トマト</td>
+                    <td>野菜</td>
+                    <td>120</td>
+                    <td>100</td>
+                    <td>20</td>
+                    <td>
+                        <button class="edit">編集</button>
+                        <button class="delete">削除</button>
+                    </td>
+                </tr> -->
+
+</table>
+</div>
+</form>
 </main>
