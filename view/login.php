f69678d4d3e986adb0cfe74192e6ac6093607630
DaikiKamada <130561768+nagoshi19th@users.noreply.github.com>
2024-11-20 12:34:43 +0900

    パスワード周りのバリデーションを強化しました^^b

--- view/login.php
+++ view/login.php
@@ -5,8 +5,11 @@
             <label for="mailAddress">メールアドレス</label>
             <input type="email" id="mailAddress" name="mailAddress" required title="有効なメールアドレスを入力してください">
 
-            <label for="userId">パスワード</label>
-            <input type="password" id="password" name="password" required pattern="" title="パスワードは半角英数・記号のみ使用できます">            
+            <label for="password">パスワード</label>
+            <input type="password" id="password" name="password" required 
+                pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{3,20}$"
+                minlength="3" maxlength="20"
+                title="パスワードは半角英字、数字、記号を含む3〜20文字で入力してください">
             <button type="submit" id="submitBtn" disabled>ログイン</button>
         </form>
     </div>
