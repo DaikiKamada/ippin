deae7e0e8304574611b130995477bd03e33c4750
DaikiKamada <130561768+nagoshi19th@users.noreply.github.com>
2024-11-20 16:58:54 +0900

    へんなところまでGM

--- foodsManagement.php
+++ foodsManagement.php
@@ -1,65 +1,84 @@
 <?php
+
+// セッションの開始
 session_start();
 
+// ファイルのインクルード
 require_once "view/View.php";
+require_once "common/InsertSql.php";
 require_once "common/SelectSql.php";
-require_once "common/insertSql.php";
 require_once 'common/Utilities.php';
 
+// $_POSTを受ける変数の準備・初期化
+$foodInfo = [];
+
 // insert(追加ボタンを押した場合の処理)
-if(array_key_exists('insert',$_POST)) {
-
-    // POSTの内容を$foodInfoにコピー
-    $foodInfo = $_GET;
-    $foodInfo['userId'] = $_SESSION['viewAry']['userId'];
-
-    // 追加処理
-    $obj = new InsertSql('食材の追加処理', 0);
-    $foodsList = $obj->insertFoodM($foodInfo);
-
-    // $_POSTを初期化
-    $_GET = array();
-
-    // 処理結果
-    $result = $foodsList->getResult();
-    if($result['resultNo'] == 0) {
-        print 'test2';
-        // 追加できなかったよというJSのアラートがほしいな～
-    }
-    else {
-        print 'test1';
-        // 追加しましたよというJSのアラートがほしいな～
-    }
-
-}
-else {
-    $recipeInfo = [];
+if (isset($_POST['insert'])) {    
+    // $_POSTの内容を$foodInfoに格納
+    $foodInfo[] = [
+        'foodName' => e($_POST['foodName']),
+        'foodCatId' => e($_POST['foodCatId'])
+    ];
 }
 
+//     // 追加処理
+//     $obj = new InsertSql('食材の追加処理', 0);
+//     $foodsList = $obj->insertFoodM($foodInfo);
 
-// 食材一覧を取得
-$obj = new SelectSql('食材一覧を取得', 0);
-$foodsList = $obj->getFood();
+//     // $_POSTを初期化
+//     $_POST = [];
 
+//     // 処理結果
+//     $result = $foodsList->getResult();
+//     if($result['resultNo'] == 0) {
+//         print 'test2';
+//         // 追加できなかったよというJSのアラートがほしいな～
+//     } else {
+//         print 'test1';
+//         // 追加しましたよというJSのアラートがほしいな～
+//     }
+// } else {
+//     $recipeInfo = [];
+// }
 
+// // 食材一覧を取得
+// $obj = new SelectSql('食材一覧を取得', 0);
+// $foodsList = $obj->getFood();
+
+// viewクラスの呼び出し
 $vi = new View();
 
+// $viに値を入れていく
 $vi->setAssign("title", "ippin管理画面 | 食材マスタ管理画面");
 $vi->setAssign("cssPath", "css/admin.css");
 $vi->setAssign("bodyId", "foodsManagement");
 $vi->setAssign("h1Title", "食材マスタ管理画面");
 $vi->setAssign("main", "foodsManagement");
-$vi->setAssign("userId", 1);
-$vi->setAssign("foodsList", $foodsList);
 
+$vi->setAssign("userId", 1);
+// $vi->setAssign("foodsList", $foodsList);
+
+// $viの値を$_SESSIONに渡して使えるようにする
 $_SESSION['viewAry'] = $vi->getAssign();
 
+// templateUserに$viを渡す
 $vi ->screenView("templateAdmin");
 
 // デバッグ用※あとで消そうね！
-// echo '<pre>';
-
-// echo '$foodsListの配列';
-// print_r($foodsList);
-
-// echo '</pre>';
+echo '<pre>';
+echo '$_SESSIONの配列';
+print_r($_SESSION);
+echo '<br>';
+echo '$_POSTの配列';
+print_r($_POST);
+echo '<br>';
+echo '$foodInfoの配列';
+print_r($foodInfo);
+echo '<br>';
+echo '$foodNameの配列';
+print_r($foodName);
+echo '<br>';
+echo '$foodCatIdの配列';
+print_r($foodCatId);
+echo '<br>';
+echo '</pre>';
--- view/foodsManagement.php
+++ view/foodsManagement.php
@@ -11,59 +11,38 @@
                 <option value="3">その他</option>
             </select>
         </div>
-        <button type="submit" id="submitBtn" class="disabled" name="insert" disabled>追加</button>
+        <button type="submit" id="submitBtn" class="disabled" name="insert" value="add" disabled>追加</button>
     </form>
     <hr>
+    
     <form method="post" id="fmTable">
         <div class="fM_containor">
             <table class="fM">
                 <tr><th>ID</th><th>食材</th><th>カテゴリ</th><th>recipe件数</th><th>表示</th><th>非表示</th><th>操作</th></tr>
-                
                 <!-- 食材一覧を表示 -->
                 <?php
-                if (isset($vAry['foodsList'])) {
-                    $foodsList = $vAry['foodsList'];
-                } else {
-                    $foodsList = [];
-                }
+                    if (isset($vAry['foodsList'])) {
+                        $foodsList = $vAry['foodsList'];
+                    } else {
+                        $foodsList = [];
+                    }
                 ?>
-                <?php
-                for($i = 0; $i < count($foodsList); $i++) {
-                    ?>
+                <?php for($i = 0; $i < count($foodsList); $i++) { ?>
                     <tr>
                         <td><?=$foodsList[$i]['foodId']?></td>
                         <td><?=$foodsList[$i]['foodName']?></td>
                         <td><?=$foodsList[$i]['catName']?></td>
-                        
                         <!-- 調整予定 -->
                         <td>120</td>
                         <td>0</td>
                         <td>120</td>
-                        
                         <td>
                             <button class="edit">編集</button>
                             <button class="delete">削除</button>
                         </td>
-                        
-                    </tr> <?php
-                }
-                ?>
-
-                <!-- サンプル行 -->
-                <!-- <tr>
-                    <td>1</td>
-                    <td>トマト</td>
-                    <td>野菜</td>
-                    <td>120</td>
-                    <td>100</td>
-                    <td>20</td>
-                    <td>
-                        <button class="edit">編集</button>
-                        <button class="delete">削除</button>
-                    </td>
-                </tr> -->
-
-</table>
-</div>
-</form>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </form>
 </main>
