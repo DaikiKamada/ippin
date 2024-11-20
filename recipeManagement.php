0a3c0a7558e285c74f43e2ae7948deb0cb68f3a8
fukumoto <186124459+fukumotoA@users.noreply.github.com>
2024-11-20 12:29:21 +0900

    レシピをinsert出来るようにしたよ

--- recipeManagement.php
+++ recipeManagement.php
@@ -7,14 +7,14 @@ require_once "common/SelectSql.php";
 require_once "common/insertSql.php";
 require_once 'common/Utilities.php';
 require_once 'view/View.php';
-// print_r($_SESSION['result']);
 
-
-// 追加ボタンを押した場合の処理
-if (!empty($_POST['insert'])) {
+// insert(追加ボタンを押した場合の処理)
+if (array_key_exists('insert',$_POST)) {
 
     // POSTの内容を$resipeInfoにコピー
     $recipeInfo = $_POST;
+    $recipeInfo['foodIds'] = $_SESSION['viewAry']['foodIds'];
+    $recipeInfo['userId'] = $_SESSION['viewAry']['userId'];
 
     // 日時を取得して配列に追加
     $lastUpdate = getDatestr();
@@ -22,42 +22,50 @@ if (!empty($_POST['insert'])) {
     // foodIdsをソート
     $foodValues = sortFoodIds($recipeInfo['foodIds']);
     $recipeInfo['foodValues'] = $foodValues;
-    // print_r ($recipeInfo);
 
     // 追加処理
     $obj = new InsertSql('insert処理', 0);
-    $result = $obj->insertRecipeT($recipeInfo);
-    // print_r ($result->getResult());
+    $recipeList = $obj->insertRecipeT($recipeInfo);
 
-    // 追加しましたよというJSのアラートがほしいな～
+    // 処理結果
+    $result = $recipeList->getResult();
+    if($result['resultNo'] == 0) {
+        // 追加できなかったよというJSのアラートがほしいな～
+    }
+    else {
+        // 追加しましたよというJSのアラートがほしいな～
+    }
+
+}
+else {
+    $recipeInfo = [];
 }
 
 // selectでレシピ一覧をとってくる
 
-// デバッグ用
+// テスト用
 $_POST['foodIds'] = [4, 5];
 $_POST['flag'] = 0;
-// print_r ($_POST);
 
+// POSTした値をコピーする
 $foodIds = $_POST['foodIds'];
 $flag = $_POST['flag'];
 
+// foodIdをソート
 $fValueStr = sortFoodIds($foodIds);
 
 // SelectSqlでレシピ一覧を取得
 $obj = new SelectSql('レシピ一覧を取得', 0);
-$result = $obj->getRecipe($fValueStr, $flag);
-// print_r($result);
+$recipeList = $obj->getRecipe($fValueStr, $flag);
 
-// foreach($result as $x) {
-//     $foodIds[$x] = explodeFoodValues($result[$x]['foodValues']);
+// 食材一覧を表示
+// foreach($recipeList as $x) {
+//     $foodIds[$x] = explodeFoodValues($recipeList[$x]['foodValues']);
 // }
 
-for($i = 0; $i < count($result); $i++) {
-    $foodIds[$i] = explodeFoodValues($result[$i]['foodValues']);
-}
-
-
+// for($i = 0; $i < count($recipeList); $i++) {
+//     $foodIds[$i] = explodeFoodValues($recipeList[$i]['foodValues']);
+// }
 
 $vi = new View();
 
@@ -66,21 +74,27 @@ $vi->setAssign("cssPath", "css/admin.css");
 $vi->setAssign("bodyId", "recipeManagement");
 $vi->setAssign("h1Title", "レシピテーブル管理画面");
 $vi->setAssign("main", "recipeManagement");
-$vi->setAssign("result", $result);
+$vi->setAssign("userId", 1);
+$vi->setAssign("recipeList", $recipeList);
+$vi->setAssign("foodIds", $foodIds);
 
 $_SESSION['viewAry'] = $vi->getAssign();
 
 $vi ->screenView("templateAdmin");
 
 // デバッグ用※あとで消そうね！
-echo '<pre>';
-echo '$_SESSIONの配列';
-print_r($_SESSION['result']);
-echo '$_SESSIONの配列';
-print_r($_SESSION);
-echo '$resultの配列';
-print_r($result);
-echo '$foodIdsの配列';
-print_r($foodIds);
+// echo '<pre>';
+// echo '$_SESSIONの配列';
+// print_r($_SESSION['viewAry']['recipeList']);
+// echo '$_SESSIONの配列';
+// print_r($_SESSION);
+// echo '$resultの配列';
+// print_r($result);
+// // echo '$foodIdsの配列';
+// // print_r($foodIds);
+// echo '$recipeInfoの配列';
+// print_r($recipeInfo);
+// echo '$_POSTの配列';
+// print_r($_POST);
 
-echo '</pre>';
+// echo '</pre>';
