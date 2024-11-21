91e96bc9a8749fb6ca44828c1ed5509e57fc0400
fukumoto <186124459+fukumotoA@users.noreply.github.com>
2024-11-21 15:02:15 +0900

    編集はまだできません

--- recipeEdit.php
+++ recipeEdit.php
@@ -2,16 +2,41 @@
 session_start();
 
 require_once 'common/UpdateSql.php';
+require_once 'common/SelectSql.php';
 require_once 'common/Utilities.php';
 require_once 'view/View.php';
 
-// 仮データ
-$_POST = []
-
+// POSTとSESSIONをコピー
 print_r($_POST);
 
-$_POST['choicedRecipe'];
+$recipeIds = $_POST['choicedRecipe'];
+$recipeInfo = $_SESSION['viewAry']['recipeList'];
 
+// 全ての食材を取得
+$selectFoods = new SelectSql('全ての食材を取得', 0);
+$allFoodsList = $selectFoods->getFood();
+
+// 配列を用意(編集したいレシピを入れる)
+$editedRecipe = [];
+
+// 編集したいレシピの一覧を取得
+for($i = 0; $i < count($recipeInfo); $i++) {
+    for($x = 0; $x < count($recipeIds); $x++) {
+        if($recipeInfo[$i]['recipeId'] == $recipeIds[$x]) {
+            $editedRecipe[] = $recipeInfo[$i];
+        }
+    }
+}
+
+// 編集ボタンが押されたら、recipteTableを更新してrecipeManagementに戻る
+if(array_key_exists('update', $_POST)) {
+    $updateRecipe = new UpdateSql('レシピを更新', 0);
+
+    // 複数のレコードを更新する
+    $result = $updateRecipe->updateRecipeT($editedRecipe);
+
+    // 処理結果に応じての処理ができたらいいな～
+}
 
 $vi = new View();
 
@@ -20,14 +45,15 @@ $vi->setAssign("cssPath", "css/admin.css");
 $vi->setAssign("bodyId", "recipeEdit");
 $vi->setAssign("h1Title", "レシピ編集画面");
 $vi->setAssign("main", "recipeEdit");
+$vi->setAssign("editedRecipe", $editedRecipe);
+$vi->setAssign("allFoodsList", $allFoodsList);
 
 $_SESSION['viewAry'] = $vi->getAssign();
 
 $vi ->screenView("templateAdmin");
 
 // デバッグ用※あとで消そうね！
-echo '<pre>';
-
+// echo '<pre>';
 // echo '$_POSTの配列';
 // print_r($_POST);
 // echo '<br>';
@@ -41,11 +67,10 @@ echo '<pre>';
 // print_r($foodIds);
 // echo '$recipeInfoの配列';
 // print_r($recipeInfo);
-echo '$_POSTの配列';
-print_r($_POST);
+// echo '$_POSTの配列';
+// print_r($_POST);
 // echo '$foodsListの配列';
 // print_r($foodsList);
 // echo '$recipeListの配列';
 // print_r($recipeList);
-
-echo '</pre>';
+// echo '</pre>';
--- view/recipeEdit.php
+++ view/recipeEdit.php
@@ -1,226 +1,98 @@
 <main>
     <h1>recipe編集</h1>
     <hr>
-    <form action="recipeManagement.php" method="post" class="recipeEdit">
+    <form action="recipeEdit.php" method="post" class="recipeEdit">
         <section class="accordion">
-            <input id="block-01" type="checkbox" class="toggle">
-            <label class="Label" for="block-01">1｜トマト煮込み｜作成者</label>
-            <div class="edit_containor">
-                <div>
-                    <label>recipe名：</label>
-                    <input type="text" name="recipeName">
-                </div>
-                
-                <div class="dropdown">
-                    <button type="button" id="dropdownButton">食材を選択（3つまで）</button>
-                    <div class="dropdown-content">
-                        <label><input type="checkbox" value="みかん" onclick="limitCheckboxes(this)"> みかん</label>
-                        <label><input type="checkbox" value="キャベツ" onclick="limitCheckboxes(this)"> キャベツ</label>
-                        <label><input type="checkbox" value="タマゴ" onclick="limitCheckboxes(this)"> タマゴ</label>
-                        <label><input type="checkbox" value="タマネギ" onclick="limitCheckboxes(this)"> タマネギ</label>
-                        <label><input type="checkbox" value="レタス" onclick="limitCheckboxes(this)"> レタス</label>
-                    </div>
-                    <!-- これに置き換える -->
-                    <div class="dropdown-content">
-                        <?php
-                            // $vAry[]にfoodsListがあれば$foodsListに配列を渡す、なければ空の配列を生成
-                            if (isset($vAry['foodsList'])) {
-                                $foodsList = $vAry['foodsList'];
-                            } else {
-                                $foodsList = [];
-                            }
-                        ?>
-                        <?php foreach ($foodsList as $f) { ?>                            
-                            <label for="foods<?= $f['foodId'] ?>">
-                                <input type="checkbox" id="foods<?= $f['foodId'] ?>" name="selectFoods[]" value="<?= $f['foodId'] ?>" onclick="limitCheckboxes(this)"><?= $f['foodName'] ?>
-                            </label>
-                        <?php } ?>
-                    </div>
-                </div>
-
-                <div>
-                    <label>調理方法：</label>
-                    <select name="howtoId">
-                        <option value="1">焼く</option>
-                        <option value="2">煮る</option>
-                        <option value="3">揚げる</option>
-                    </select>
-                </div>
-
-                <div class="full-width">
-                    <label>コメント：</label>
-                    <textarea name="comment"></textarea>
-                </div>
-                
-                <div>
-                    <label>補足：</label>
-                    <input type="text" name="supplement">
-                </div>
-                
-                <div>
-                    <label>recipe画像をアップロード</label>
-                    <input type="file" name="img">
-                </div>
-                
-                <div>
-                    <label>recipeリンク：</label>
-                    <input type="text" name="url" >
-                </div>
-
-                <div>
-                    <label>出典元：</label>
-                    <input type="text" name="siteName">
-                </div>
-            
-                <div>
-                    <label>表示設定：</label>
+            <?php
+            if (isset($vAry['editedRecipe'])) {
+                $editedRecipe = $vAry['editedRecipe'];
+            }
+            else {
+                $editedRecipe = [];
+            }
+            ?>
+            <?php
+            for($i = 0; $i < count($editedRecipe); $i++) {
+                ?><input id="block-<?= $i ?>" type="checkbox" class="toggle">
+                <label class="Label" for="block-<?= $i ?>"><?= $editedRecipe[$i]['recipeName'] ?></label>
+                <div class="edit_containor">
                     <div>
-                        <input type="radio" id="show" name="recipeFlag" value="show" checked/>
-                        <label for="show">表示</label>
-                        <input type="radio" id="hide" name="recipeFlag" value="hide" />
-                        <label for="hide">非表示</label>
+                        <label>recipe名：</label>
+                        <input type="text" name="recipeName">
+                    </div>
+                    
+                    <div class="dropdown">
+                        <button type="button" id="dropdownButton">食材を選択（3つまで）</button>
+                        <div class="dropdown-content">
+                            <?php
+                                // $vAry[]にfoodsListがあれば$foodsListに配列を渡す、なければ空の配列を生成
+                                if (isset($vAry['allFoodsList'])) {
+                                    $allFoodsList = $vAry['allFoodsList'];
+                                } else {
+                                    $allFoodsList = [];
+                                }
+                            ?>
+                            <?php foreach ($allFoodsList as $f) { ?>                            
+                                <label for="foods<?= $f['foodId'] ?>">
+                                    <input type="checkbox" id="foods<?= $f['foodId'] ?>" name="selectFoods[]" value="<?= $f['foodId'] ?>"><?= $f['foodName'] ?>
+                                </label>
+                            <?php }?>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label>調理方法：</label>
+                        <select name="howtoId">
+                            <option value="1">焼く</option>
+                            <option value="2">煮る</option>
+                            <option value="3">揚げる</option>
+                        </select>
+                    </div>
+
+                    <div class="full-width">
+                        <label>コメント：</label>
+                        <textarea name="comment"></textarea>
+                    </div>
+                    
+                    <div>
+                        <label>補足：</label>
+                        <input type="text" name="memo">
+                    </div>
+                    
+                    <div>
+                        <label>recipe画像をアップロード</label>
+                        <input type="file" name="img">
+                    </div>
+                    
+                    <div>
+                        <label>recipeリンク：</label>
+                        <input type="text" name="url" >
+                    </div>
+
+                    <div>
+                        <label>出典元：</label>
+                        <input type="text" name="siteName">
+                    </div>
+
+                    <div>
+                        <label>表示設定：</label>
+                        <div>
+                            <input type="radio" id="show" name="recipeFlag" value="show" checked/>
+                            <label for="show">表示</label>
+                            <input type="radio" id="hide" name="recipeFlag" value="hide" />
+                            <label for="hide">非表示</label>
+                        </div>
                     </div>
                 </div>
-            </div>
-
+                <?php } ?>
             <hr>
-            
-            <input id="block-02" type="checkbox" class="toggle">
-            <label class="Label" for="block-02">2｜ハンバーガー｜作成者</label>
-            <div class="edit_containor">
-                <div>
-                    <label>recipe名：</label>
-                    <input type="text" name="recipeName">
-                </div>
-                
-                <div class="dropdown">
-                    <button type="button" id="dropdownButton">食材を選択（3つまで）</button>
-                    <div class="dropdown-content">
-                        <label><input type="checkbox" value="みかん" onclick="limitCheckboxes(this)"> みかん</label>
-                        <label><input type="checkbox" value="キャベツ" onclick="limitCheckboxes(this)"> キャベツ</label>
-                        <label><input type="checkbox" value="タマゴ" onclick="limitCheckboxes(this)"> タマゴ</label>
-                        <label><input type="checkbox" value="タマネギ" onclick="limitCheckboxes(this)"> タマネギ</label>
-                        <label><input type="checkbox" value="レタス" onclick="limitCheckboxes(this)"> レタス</label>
-                    </div>
-                </div>
-
-                <div>
-                    <label>調理方法：</label>
-                    <select name="howtoId">
-                        <option value="1">焼く</option>
-                        <option value="2">煮る</option>
-                        <option value="3">揚げる</option>
-                    </select>
-                </div>
-
-                <div class="full-width">
-                    <label>コメント：</label>
-                    <textarea name="comment"></textarea>
-                </div>
-                
-                <div>
-                    <label>補足：</label>
-                    <input type="text" name="supplement">
-                </div>
-                
-                <div>
-                    <label>recipe画像をアップロード</label>
-                    <input type="file" name="img">
-                </div>
-                
-                <div>
-                    <label>recipeリンク：</label>
-                    <input type="text" name="url" >
-                </div>
-
-                <div>
-                    <label>出典元：</label>
-                    <input type="text" name="siteName">
-                </div>
-            
-                <div>
-                    <label>表示設定：</label>
-                    <div>
-                        <input type="radio" id="show" name="recipeFlag" value="show" checked/>
-                        <label for="show">表示</label>
-                        <input type="radio" id="hide" name="recipeFlag" value="hide" />
-                        <label for="hide">非表示</label>
-                    </div>
-                </div>
-            </div>
-
-            <hr>
-
-            <input id="block-03" type="checkbox" class="toggle">
-            <label class="Label" for="block-03">3｜もつ鍋｜作成者</label>
-            <div class="edit_containor">
-                <div>
-                    <label>recipe名：</label>
-                    <input type="text" name="recipeName">
-                </div>
-                
-                <div class="dropdown">
-                    <button type="button" id="dropdownButton">食材を選択（3つまで）</button>
-                    <div class="dropdown-content">
-                        <label><input type="checkbox" value="みかん" onclick="limitCheckboxes(this)"> みかん</label>
-                        <label><input type="checkbox" value="キャベツ" onclick="limitCheckboxes(this)"> キャベツ</label>
-                        <label><input type="checkbox" value="タマゴ" onclick="limitCheckboxes(this)"> タマゴ</label>
-                        <label><input type="checkbox" value="タマネギ" onclick="limitCheckboxes(this)"> タマネギ</label>
-                        <label><input type="checkbox" value="レタス" onclick="limitCheckboxes(this)"> レタス</label>
-                    </div>
-                </div>
-
-                <div>
-                    <label>調理方法：</label>
-                    <select name="howtoId">
-                        <option value="1">焼く</option>
-                        <option value="2">煮る</option>
-                        <option value="3">揚げる</option>
-                    </select>
-                </div>
-
-                <div class="full-width">
-                    <label>コメント：</label>
-                    <textarea name="comment"></textarea>
-                </div>
-                
-                <div>
-                    <label>補足：</label>
-                    <input type="text" name="supplement">
-                </div>
-                
-                <div>
-                    <label>recipe画像をアップロード</label>
-                    <input type="file" name="img">
-                </div>
-                
-                <div>
-                    <label>recipeリンク：</label>
-                    <input type="text" name="url" >
-                </div>
-
-                <div>
-                    <label>出典元：</label>
-                    <input type="text" name="siteName">
-                </div>
-            
-                <div>
-                    <label>表示設定：</label>
-                    <div>
-                        <input type="radio" id="show" name="recipeFlag" value="show" checked/>
-                        <label for="show">表示</label>
-                        <input type="radio" id="hide" name="recipeFlag" value="hide" />
-                        <label for="hide">非表示</label>
-                    </div>
-                </div>
-            </div>
         </section>
         
         <hr>
 
         <div class="editCheck">
             <button type="button" onclick="location.href='recipeManagement.php'">キャンセル</button>
-            <button type="submit">変更</button>
+            <button type="submit" name="update" value="write">変更</button>
         </div>
     </form>
 </main>
