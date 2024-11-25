810a1ff5b2c7dc49f7ddd167642ad8e6bdaaec06
DaikiKamada <130561768+nagoshi19th@users.noreply.github.com>
2024-11-25 13:54:06 +0900

    新規ページの型枠push

---
+++ view/lcFoodsEdit.php
@@ -0,0 +1,49 @@
+<main>
+    <h1>食材編集</h1>
+    <hr>
+    <form id="foodForm" action="foodsEdit.php" method="POST">
+        <div class="data_containor">
+            <table class="data" border="1">
+                <tr><th>ID</th><th>食材</th><th>カテゴリ</th><th>recipe件数</th><th>表示</th><th>非表示</th></tr>
+                <!-- 食材名を表示 -->
+                <?php
+                    if (isset($vAry['editInfo'])) {
+                        $editInfo = $vAry['editInfo'];
+                    } else {
+                        $editInfo = [];
+                    }
+                ?>
+                <tr>
+                    <td><?=$editInfo['foodId']?></td>
+                    <td><input type="text" name="foodName" value="<?=$editInfo['foodName']?>" class="tdinput"></td>
+                    <!-- <td><select class="tdinput" name="foodCatId">
+                        <option value="1">肉</option>
+                        <option value="2">野菜</option>
+                        <option value="3">その他</option>
+                    </select></td> -->
+                    <td><select name="foodCatId" value="<?=$editInfo['catName']?>">
+                        <!-- 食材カテゴリを表示 -->
+                        <?php
+                            if (isset($vAry['foodCatM'])) {
+                                $foodCatM = $vAry['foodCatM'];
+                            } else {
+                                $foodCatM = [];
+                            }
+                        ?>
+                        <?php for($i = 0; $i < count($foodCatM); $i++) { ?>
+                            <option value="<?=$foodCatM[$i]['foodCatId']?>"><?=$foodCatM[$i]['catName']?></option>
+                        <?php } ?>
+                    </select></td>
+                    <td><?=$editInfo['recipe_count']?></td>
+                    <td><?=$editInfo['flag1_count']?></td>
+                    <td><?=$editInfo['flag0_count']?></td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="editCheck">
+            <button class="edit" type="button" name="update" value="cancel" onclick="location.href='foodsManagement.php'">キャンセル</button>
+            <button class="delete" type="submit" name="update" value="update">変更</button>
+        </div>
+    </form>
+</main>
