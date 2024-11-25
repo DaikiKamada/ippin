e739ec8106bba918a88b2ffd0d68d28e9c545478
DaikiKamada <130561768+nagoshi19th@users.noreply.github.com>
2024-11-25 16:52:49 +0900

    福本さんにそろえた

--- recipeManagement.php
+++ recipeManagement.php
@@ -9,33 +9,17 @@ require_once 'common/ImgFile.php';
 require_once 'view/View.php';
 
 
-////////// ユーザー認証処理 //////////
-// セッション情報から認証情報を取得し、権限があるかをチェック
-$userMail = $_SESSION['userMail'];
-$userPw = $_SESSION['userPw'];
-$userFlag = 0;
-$obj = new UserLogin('ユーザ認証処理', 6);
+////////// insert(追加ボタンを押した場合の処理) /////////
+if (isset($_POST['insert'])) {
 
-if (isset($userMail) && isset($userPw)) {
-    // ユーザ認証を実行
-    $result = $obj->checkUserInfo($userMail, sha1($userPw), $userFlag);
-
-    if ($result) { 
-        ////////// 画面出力制御処理 //////////
-        ////////// Insert(追加ボタンを押した場合の処理) //////////
-        if (isset($_POST['insert'])) {
-
-            // POSTとFILESの内容を$resipeInfoにコピー
-            $recipeInfo = $_POST;
-            $FileInfo = $_FILES;
-
-            $fileCheckObj = new ImgFile('画像ファイル処理', 0);
-            $NewRecipeId = $fileCheckObj->getNewRecipeId();
-            $fileCheck = $fileCheckObj->checkUplodeFile($NewRecipeId, $FileInfo);
-
-    // POSTの内容を$resipeInfoにコピー
+    // POSTとFILESの内容を$resipeInfoにコピー
     $recipeInfo = $_POST;
     $FileInfo = $_FILES;
+
+    $fileCheckObj = new ImgFile('画像ファイル処理', 0);
+    $NewRecipeId = $fileCheckObj->getNewRecipeId();
+    $fileCheck = $fileCheckObj->checkUplodeFile($NewRecipeId, $FileInfo);
+
     $recipeInfo['foodIds'] = $_SESSION['viewAry']['foodIds'];
     $recipeInfo['userId'] = $_SESSION['userId'];
     $recipeInfo['img'] = $fileCheck;
@@ -67,109 +51,96 @@ if (isset($userMail) && isset($userPw)) {
         if ($result['resultNo'] == 0) {
             print_r($result);
 
-                    } else {
-                        // SQLが正常実行の場合、画像ファイルをアップロード
-                        // エラーの場合、ファイルの修正を促すメッセージを表示
-                        $fileUp = $fileCheckObj->fileUplode($img);
-
-                        if (checkClass($fileUp)) {
-                            //エラー画面に遷移？
-                            $resultArr = $fileUp->getResult();
-
-                        }
-                    }
-                }
-            } 
         } else {
-            $recipeInfo = [];
+            // SQLが正常実行の場合、画像ファイルをアップロード
+            // エラーの場合、ファイルの修正を促すメッセージを表示
+            $fileUp = $fileCheckObj->fileUplode($img, $FileInfo);
 
-        }
+            if (checkClass($fileUp)) {
+                //エラー画面に遷移？
+                $resultArr = $fileUp->getResult();
 
-
-        // ////////// selectでレシピ一覧を取得 //////////
-        $foodIds = [];
-
-        // SESSIONの['viewAry']に['foodIds']と['flag']があればコピーし、なければPOSTの中身をコピーする
-        if(array_key_exists('foodIds',$_SESSION['viewAry']) && array_key_exists('flag', $_SESSION['viewAry'])) {
-            $foodIds = $_SESSION['viewAry']['foodIds'];
-            $flag = $_SESSION['viewAry']['flag'];
-
-        } else {
-            $foodIds = $_POST['selectFoods'];
-            $flag = $_POST['flag'];
-            
-        }
-
-        // foodIdsをソート
-        $fValueStr = sortFoodIds($foodIds);
-
-        // SelectSqlでレシピ一覧を取得
-        $obj = new SelectSql('レシピ一覧を取得', 0);
-        $recipeList = $obj->getRecipe($fValueStr, $flag);
-
-        if(checkClass($recipeList)) {
-            // レシピがなかった場合の処理
-            print_r($recipeList);
-            echo '<a href="manageTop.php">戻る</a>';
-
-        } else {
-            // レシピがあった場合の処理
-
-            // // recipeFlagを上書きする(0を非表示に、1を表示に、そのほかの場合は不明に)
-            for ($i = 0; $i < count($recipeList); $i++) {
-                if ($recipeList[$i]['recipeFlag'] == 0) {
-                    $recipeList[$i]['recipeFlag'] = '非表示';
-
-                } elseif ($recipeList[$i]['recipeFlag'] == 1) {
-                    $recipeList[$i]['recipeFlag'] = '表示';
-
-                } else {
-                    $recipeList[$i]['recipeFlag'] = '不明';
-
-                }
-            }
-            
-            // SelectSqlで食材名を取得
-            $foodsList = $obj->getSelectedFood($foodIds);
-            
-            if(checkClass($recipeList)) {
-                ///////////////////////////////// true : エラー処理する /////////////////////////////////
-                echo '<p>たいへん！食材がうまく取得できないよ！管理人を呼んでね！</p>';
-
-            } else {
-                // viewクラスの呼び出し
-                $vi = new View();
-                
-                // $viに値を入れていく
-                $vi->setAssign("title", "ippin管理画面 | レシピテーブル管理画面");
-                $vi->setAssign("cssPath", "css/admin.css");
-                $vi->setAssign("bodyId", "recipeManagement");
-                $vi->setAssign("h1Title", "レシピテーブル管理画面");
-                $vi->setAssign("main", "recipeManagement");
-                $vi->setAssign("foodIds", $foodIds);
-                $vi->setAssign("flag", $flag);
-                $vi->setAssign("recipeList", $recipeList);
-                $vi->setAssign("foodsList", $foodsList);
-                
-                // $viの値を$_SESSIONに渡して使えるようにする
-                $_SESSION['viewAry'] = $vi->getAssign();
-                
-                // templateUserに$viを渡す
-                $vi ->screenView("templateAdmin");
-                
             }
         }
-    } else {
-        $vi = $obj->getLoginErrView();
-        $_SESSION['viewAry'] = $vi->getAssign();
-        $vi->screenView('templateAdmin');
-    
     }
 } else {
-    $vi = $obj->getLoginErrView();
-    $_SESSION['viewAry'] = $vi->getAssign();
-    $vi->screenView('templateAdmin');
+    $recipeInfo = [];
 
+}
+
+
+// ////////// selectでレシピ一覧を取得 //////////
+$foodIds = [];
+
+// SESSIONの['viewAry']に['foodIds']と['flag']があればコピーし、なければPOSTの中身をコピーする
+if(array_key_exists('foodIds',$_SESSION['viewAry']) && array_key_exists('flag', $_SESSION['viewAry'])) {
+    $foodIds = $_SESSION['viewAry']['foodIds'];
+    $flag = $_SESSION['viewAry']['flag'];
+
+} else {
+    $foodIds = $_POST['selectFoods'];
+    $flag = $_POST['flag'];
+    
+}
+
+// foodIdsをソート
+$fValueStr = sortFoodIds($foodIds);
+
+// SelectSqlでレシピ一覧を取得
+$obj = new SelectSql('レシピ一覧を取得', 0);
+$recipeList = $obj->getRecipe($fValueStr, $flag);
+
+if(checkClass($recipeList)) {
+    // レシピがなかった場合の処理
+    print_r($recipeList);
+    echo '<a href="manageTop.php">戻る</a>';
+
+} else {
+    // レシピがあった場合の処理
+
+    // // recipeFlagを上書きする(0を非表示に、1を表示に、そのほかの場合は不明に)
+    for ($i = 0; $i < count($recipeList); $i++) {
+        if ($recipeList[$i]['recipeFlag'] == 0) {
+            $recipeList[$i]['recipeFlag'] = '非表示';
+
+        } elseif ($recipeList[$i]['recipeFlag'] == 1) {
+            $recipeList[$i]['recipeFlag'] = '表示';
+
+        } else {
+            $recipeList[$i]['recipeFlag'] = '不明';
+
+        }
+    }
+    
+    // SelectSqlで食材名を取得
+    $foodsList = $obj->getSelectedFood($foodIds);
+    
+    if(checkClass($recipeList)) {
+        ///////////////////////////////// true : エラー処理する /////////////////////////////////
+        echo '<p>たいへん！食材がうまく取得できないよ！管理人を呼んでね！</p>';
+
+    } else {
+        // viewクラスの呼び出し
+        $vi = new View();
+        
+        // $viに値を入れていく
+        $vi->setAssign("title", "ippin管理画面 | レシピテーブル管理画面");
+        $vi->setAssign("cssPath", "css/admin.css");
+        $vi->setAssign("bodyId", "recipeManagement");
+        $vi->setAssign("h1Title", "レシピテーブル管理画面");
+        $vi->setAssign("main", "recipeManagement");
+        $vi->setAssign("foodIds", $foodIds);
+        $vi->setAssign("flag", $flag);
+        $vi->setAssign("recipeList", $recipeList);
+        $vi->setAssign("foodsList", $foodsList);
+        
+        // $viの値を$_SESSIONに渡して使えるようにする
+        $_SESSION['viewAry'] = $vi->getAssign();
+        
+        // templateUserに$viを渡す
+        $vi ->screenView("templateAdmin");
+        
+    }
 }
 
 
