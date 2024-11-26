1dc323960e962df29e25b3f7dbaa63fd80e72c3f
cNagata-Code <186126038+cNagata-Code@users.noreply.github.com>
2024-11-26 12:24:02 +0900

    リンク切れ削除処理を修正 @nagata

--- lcRecipeDeleteCheck.php
+++ lcRecipeDeleteCheck.php
@@ -31,6 +31,7 @@ if (isset($_SESSION['userMail']) && isset($_SESSION['userPw'])) {
         if (isset($_POST['choicedRecipe'])) {
             $recipeIds = $_POST['choicedRecipe'];
             $recipeInfo = $_SESSION['viewAry']['noLinkRecipeList'];
+            // 削除のチェックがついたレシピのみ配列にセット
             for ($i = 0; $i < count($recipeInfo); $i++) {
                 for ($x = 0; $x < count($recipeIds); $x++) {
                     if ($recipeInfo[$i]['recipeId'] == $recipeIds[$x]) {
@@ -38,7 +39,7 @@ if (isset($_SESSION['userMail']) && isset($_SESSION['userPw'])) {
                     }
                 }
             }
-            // $viに値を入れていく
+            // 確認画面の値をViewインスタンへセット
             $vi->setAssign('title', 'ippin管理画面 | リンク切れレシピ削除確認画面');
             $vi->setAssign('cssPath', 'css/admin.css');
             $vi->setAssign('bodyId', 'lcRecipeDeleteCheck');
@@ -52,102 +53,73 @@ if (isset($_SESSION['userMail']) && isset($_SESSION['userPw'])) {
             // $viの値を$_SESSIONに渡して使えるようにする
             $_SESSION['viewAry'] = $vi->getAssign();
     
-            // templateUserに$viを渡す
+            // templateAdminに$viを渡す
             $vi ->screenView('templateAdmin');
             exit;
 
-        } else {
+        } else { //削除処理を実行する場合
             $recipeIds = $_SESSION['viewAry']['recipeIds'];
             foreach ($_SESSION['viewAry']['deleteRecipe'] as $info) {
                 $recipeInfo[$info['recipeId']] = $info;
             }
-            
-        }
-        
-        
-        
-        // 削除ボタンが押されたら、recipeTableを更新して処理結果画面へ遷移
-        if (array_key_exists('delete', $_POST)) {
-            if ($_POST['delete'] == 'delete') {
-                
-                $deletedRecipe = new DeleteSql('リンク切れレシピを削除', 0);
-                
-                // 複数のレコードを更新する
-                $result = $deletedRecipe->deleteRecipeT($recipeIds);
-                $resultArr = $result;
-                // 失敗したらエラー画面へ遷移
-                $checkCount = 1;
-                $htmlText = '';
-                foreach ($resultArr as $key => $value) {
-                    if (checkClass($value)) {
-                        $resultObj = $value->getResult();
-                        if ($resultObj['resultNo'] == 0) {
-                            $checkCount = 0;
-                        }
-                        //テーブルデータを作成
-                        $htmlText .= '<tr>
-                            <td>' . ($resultObj['resultNo'] == 0? '失敗':'成功') . '</td>'.
-                            '<td>' . ($recipeInfo[$key]['recipeName']) . '</td>'.
-                            '<td>' . ($resultObj['resultMsg']) . '</td></tr>';
-                    }
-                }
-                // $htmlText .= '<table><tr><th></th><th>レシピ名</th><th>処理結果</th></tr>'
-                //     .$htmlText.'</table>';
+            // 削除ボタンが押されたら、recipeTableを更新して処理結果画面へ遷移
+            if (array_key_exists('delete', $_POST)) {
+                if ($_POST['delete'] == 'delete') {
+                    // レコードを削除する（複数）
+                    $deletedRecipe = new DeleteSql('リンク切れレシピを削除', 0);
+                    $result = $deletedRecipe->deleteRecipeT($recipeIds);
 
-                $vi->setAssign('title', 'ippin管理画面 | リンク切れレシピ削除結果画面'); // タイトルバー用
-                $vi->setAssign('cssPath', 'css/admin.css');  // CSSファイルの指定
-                $vi->setAssign('bodyId', 'result');  // ？
-                $vi->setAssign('main', 'result');    // テンプレート画面へインクルードするPHPファイル
-                $vi->setAssign('resultNo', $checkCount);  // 処理結果No 0:エラー, 1:成功
-                $vi->setAssign('h1Title', 'リンク切れレシピ削除結果'); // エラーメッセージのタイトル
-                $vi->setAssign('resultMsg', $htmlText); // エラーメッセージ
-                $vi->setAssign('linkUrl', 'manageTop.php');    // 戻るボタンに設置するリンク先
-        
-                // $viの値を$_SESSIONに渡して使えるようにする
-                $_SESSION['viewAry'] = $vi->getAssign();
-        
-                // templateUserに$viを渡す
-                $vi ->screenView('templateAdmin');
-                exit;
-                   
-            } elseif ($_POST['delete'] == 'cancel') {
-                // 処理をせずにmanageTopへリダイレクト
-                header('Location: manageTop.php');
-            }
-        } 
-    
-    } else {
+                    // 削除結果を処理結果画面用に加工
+                    $resultArr = $result;
+                    $checkCount = 1;
+                    $htmlText = '';
+                    foreach ($resultArr as $key => $value) {
+                        if (checkClass($value)) {
+                            $resultObj = $value->getResult();
+                            if ($resultObj['resultNo'] == 0) {
+                                $checkCount = 0;
+                            }
+                            //テーブルデータを作成
+                            $htmlText .= '<tr>
+                                <td>' . ($resultObj['resultNo'] == 0? '失敗':'成功') . '</td>'.
+                                '<td>' . ($recipeInfo[$key]['recipeName']) . '</td>'.
+                                '<td>' . ($resultObj['resultMsg']) . '</td></tr>';
+                        }
+                    }
+                    // 処理結果画面の値をViewインスタンへセット
+                    $vi->setAssign('title', 'ippin管理画面 | リンク切れレシピ削除結果画面'); // タイトルバー用
+                    $vi->setAssign('cssPath', 'css/admin.css');  // CSSファイルの指定
+                    $vi->setAssign('bodyId', 'error');  // ？
+                    $vi->setAssign('main', 'error');    // テンプレート画面へインクルードするPHPファイル
+                    $vi->setAssign('resultNo', $checkCount);  // 処理結果No 0:エラー, 1:成功
+                    $vi->setAssign('h1Title', 'リンク切れレシピ削除結果'); // エラーメッセージのタイトル
+                    $vi->setAssign('resultMsg', $htmlText); // エラーメッセージ
+                    $vi->setAssign('linkUrl', 'foodsManagement.php');    // 戻るボタンに設置するリンク先
+            
+                    // $viの値を$_SESSIONに渡して使えるようにする
+                    $_SESSION['viewAry'] = $vi->getAssign();
+                    // templateUserに$viを渡す
+                    $vi ->screenView('templateAdmin');
+                    exit;
+                    
+                } elseif ($_POST['delete'] == 'cancel') { //削除処理がキャンセルされた場合
+                    // 処理をせずにrecipeManagementへリダイレクト
+                    header('Location: recipeManagement.php');
+                }
+            } 
+        }
+
+    } else { //ユーザ認証NGの場合、エラー画面へ遷移
         $vi = $obj->getLoginErrView();
         $_SESSION['viewAry'] = $vi->getAssign();
-        $vi->screenView('templateUser');
+        $vi->screenView('templateAdmin');
     
     }
 
-} else {
+} else { //ユーザ認証用のセッション情報がない場合、エラー画面へ遷移
     $vi = $obj->getLoginErrView();
     $_SESSION['viewAry'] = $vi->getAssign();
-    $vi->screenView('templateUser');
+    $vi->screenView('templateAdmin');
 
 }
 
-
-// デバッグ用※あとで消そうね！
-echo '<pre>';
-
-echo '$_SESSIONの配列';
-print_r($_SESSION['viewAry']);
-echo '<br>';
-echo '$_POSTの配列';
-print_r($_POST);
-echo '<br>';
-echo '$recipeIdsの配列';
-print_r($recipeIds);
-echo '<br>';
-echo '$recipeInfoの配列';
-print_r($recipeInfo);
-echo '<br>';
-echo '$deleteRecipeの配列';
-print_r($deleteRecipe);
-echo '<br>';
-
-echo '</pre>';
